e3a32410cf9d8326d3229e7e4b80106c5f2fde29
Sivantha Paranavithana <sivantha.paranavithana@gmail.com>
2022-03-02 00:11:18 +0530

    Feat: Add redux, redux-saga, redux-persist, react-navigation

---
+++ template/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    printWidth: 100,
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+};
